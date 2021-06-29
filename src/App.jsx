f8ca2733061a5aa978c95389aa112c315c5fe6fe
Vadim Brikulya <im.master99@gmail.com>
2021-06-29 08:16:36 +0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Profile from "./Components/Profile/Profile";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Profile></Profile>
+    </div>
+  );
+}
+
+export default App;
