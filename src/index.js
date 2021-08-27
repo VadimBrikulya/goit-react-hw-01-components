6cb0b7bbc0857c6f5268165977dbae492f5b5932
Vadim Brikulya <im.master99@gmail.com>
2021-08-27 20:35:31 +0300

    up

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 import ReactDOM from 'react-dom';
 
-const data = {
-  id: 'id-1',
-  url: 'https://cdn.pixabay.com/photo/2017/07/31/22/05/feathers-2561511_1280.jpg',
-  title: 'Feathers. Art abstract',
-  price: 500,
-  author: {
-    tag: 'ractapopulous',
-    url: 'https://pixabay.com/users/ractapopulous-24766/',
-  },
-  quantity: 10,
-};
+import paintings from './paintings.json';
 
-const painting = (
-  <div>
-    <img src={data.url} alt={data.title} width="480" />
-    <h2>{data.title}</h2>
-    <p>
-      Автор: <a href={data.author.url}>{data.author.tag}</a>
-    </p>
-    <p>Цена: {data.price} кредитов</p>
-    <p>Доступность: заканчивается или есть в наличии</p>
-    <button type="button">Добавить в корзину</button>
-  </div>
+function Painting(props) {
+  console.log(props);
+  return (
+    <div>
+      <img src={props.url} alt={painting.title} width="480" />
+      <h2></h2>
+      <p>
+        Автор: <a href={props.profileUrl}>{props.authorName}</a>
+      </p>
+      <p>Цена: кредитов</p>
+      <p>Доступность: заканчивается или есть в наличии</p>
+      <button type="button">Добавить в корзину</button>
+    </div>
+  );
+}
+
+const painting = paintings[0];
+ReactDOM.render(
+  <Painting
+    url={painting.url}
+    title={painting.title}
+    authorName={painting.author.tag}
+    profileUrl={painting.author.url}
+  />,
+  document.querySelector('#root'),
 );
-
-ReactDOM.render(painting, document.querySelector('#root'));
