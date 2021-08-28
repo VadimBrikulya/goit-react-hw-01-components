f39400013b35cb64961f96ae6a73416bcc72b646
Vadim Brikulya <im.master99@gmail.com>
2021-08-28 16:18:38 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,30 +1,6 @@
 import ReactDOM from 'react-dom';
+import App from 'components/App';
 
-import paintings from './paintings.json';
+// const painting = paintings[2];
 
-function Painting(props) {
-  console.log(props);
-  return (
-    <div>
-      <img src={props.url} alt={painting.title} width="480" />
-      <h2></h2>
-      <p>
-        Автор: <a href={props.profileUrl}>{props.authorName}</a>
-      </p>
-      <p>Цена: кредитов</p>
-      <p>Доступность: заканчивается или есть в наличии</p>
-      <button type="button">Добавить в корзину</button>
-    </div>
-  );
-}
-
-const painting = paintings[0];
-ReactDOM.render(
-  <Painting
-    url={painting.url}
-    title={painting.title}
-    authorName={painting.author.tag}
-    profileUrl={painting.author.url}
-  />,
-  document.querySelector('#root'),
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
