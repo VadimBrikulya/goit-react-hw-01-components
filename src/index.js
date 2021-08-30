ba238f81d4d3ebf1cfafb4c1acb06e631ec98f5c
Vadim Brikulya <im.master99@gmail.com>
2021-08-30 19:04:05 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components';
+import './index.css';
+import App from 'App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
